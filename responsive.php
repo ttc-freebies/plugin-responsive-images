ea4f43f250e5a103179f6823e0499777393bf08e
dGrammatiko <mit505@upshift.gr>
2018-09-10 12:30:31 +0200

    Update responsive.php

--- responsive.php
+++ responsive.php
@@ -101,7 +101,7 @@ class PlgContentResponsive extends JPlugin
 				$filesrc = '/media/cached-resp-images/' . $dirname . '/' . $filename . '@' . $breakpoints[$i] . '.' . $extension;
 				if (file_exists(JPATH_ROOT . $filesrc))
 				{
-					$srcset .= $filesrc . ' ' . $breakpoints[$i] . 'w';
+					$srcset .= $filesrc . ' ' . $breakpoints[$i] . 'w, ';
 				}
 			}
 		}
