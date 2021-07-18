0e672a766c14a045e1889eb85f4636fa3f85af17
dgrammatiko <d.grammatiko@gmail.com>
2021-07-19 00:16:26 +0200

    fix the dir/json creation

--- src/libraries/Ttc/Freebies/Responsive/Helper.php
+++ src/libraries/Ttc/Freebies/Responsive/Helper.php
@@ -339,11 +339,11 @@ class Helper
     $file  = array_pop($parts);
     $dir   = '';
     foreach ($parts as $part) {
-      if (!is_dir($dir .= "/$part")) {
+      if ('' !== $part && !is_dir($dir .= "/$part")) {
         mkdir($dir);
       }
-      file_put_contents("$dir/$file", $contents);
     }
+    file_put_contents("$dir/$file", $contents);
   }
 
   /**
