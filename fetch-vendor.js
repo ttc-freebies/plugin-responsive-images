6c506a3b872fd88c5a05c13e4d9f2111c2cb29b5
dgrammatiko <d.grammatiko@gmail.com>
2022-03-15 02:13:23 +0100

    upd

--- fetch-vendor.js
+++ fetch-vendor.js
@@ -4,9 +4,7 @@ const { mkdir, rm, rename } = require('fs').promises;
 (async () => {
   await mkdir('build_tmp', { recursive: true });
   await mkdir('src/libraries/Ttc/vendor');
-  await mkdir('src/libraries/Ttc/vendor_prefixed');
-  await exec('git clone --depth 1 --branch prefixed git@github.com:ttc-freebies/ttc--intervention-image.git build_tmp');
+  await exec('git clone --depth 1 --branch main git@github.com:ttc-freebies/ttc--intervention-image.git build_tmp');
   await rename('build_tmp/vendor', 'src/libraries/Ttc/vendor');
-  await rename('build_tmp/vendor_prefixed', 'src/libraries/Ttc/vendor_prefixed');
   await rm('build_tmp', { recursive: true });
 })()
